f88def199e756bad57507daaef2fc5f0447c212f
nyel the runner <149019962+asapnyell@users.noreply.github.com>
2025-06-14 00:17:59 -0300

    Update Aluno.java

--- src/main/java/inicio/Aluno.java
+++ src/main/java/inicio/Aluno.java
@@ -1,5 +1,37 @@
 package main.java.inicio;
 
 public class Aluno {
+    private String nome;
+    private String sobrenome;
+    private int matricula;
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getSobrenome(){
+        return this.sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome){
+        this.sobrenome= sobrenome;
+    }
+
+    public int getMatricula(){
+        return this.matricula;
+    }
+
+    public void setMatricula(int matricula){
+        this.matricula= matricula;
+    }
+
+    public String matricular(){
+        return "Aluno matriculado: "+ this.nome + " " + this.sobrenome;
+    }
+
 
 }
