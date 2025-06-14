ac20922a11187beb5d6f2f8078a2249b173543b3
nyel the runner <149019962+asapnyell@users.noreply.github.com>
2025-06-14 00:18:02 -0300

    Update Principal.java

--- src/main/java/inicio/Principal.java
+++ src/main/java/inicio/Principal.java
@@ -1,4 +1,16 @@
 package main.java.inicio;
 
 public class Principal {
+    public static void main(String[] args) {
+
+        Aluno aluno = new Aluno();
+        aluno.setNome("Danyel");
+        aluno.setSobrenome("Henrique");
+        System.out.println(aluno.matricular());
+
+        Aluno aluno2 = new Aluno();
+        aluno2.setNome("Jessica");
+        aluno2.setSobrenome("Martinho");
+        System.out.println(aluno2.matricular());
+    }
 }
